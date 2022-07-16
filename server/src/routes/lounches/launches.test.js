29aa293471c8d7c20297659149e1c2e35aca993a
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2022-07-16 16:00:12 +0900

    pm2 installed

---
+++ server/src/routes/lounches/launches.test.js
@@ -0,0 +1,11 @@
+describe("Test Get /launches", () => {
+  test("It should respond with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /launch", () => {
+  test("It should respond with 200 success", () => {});
+  test("It should catch missing required properities", () => {});
+});
