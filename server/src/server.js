27cdacfcef6f6934bea82c3ce270664a9632989e
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2022-07-17 23:48:01 +0900

    dotenv installed

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,17 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
+
+const DB = process.env.DB;
+console.log("process.env: ", process.env);
+console.log("DB: ", DB);
+
 const startServer = async () => {
   await loadPlanetsData();
   server.listen(PORT, () => {
